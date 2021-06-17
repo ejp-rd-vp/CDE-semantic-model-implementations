c94b0309a5b61ebe8b9df4262c00296662b03f40
Mark Wilkinson <markw@illuminae.com>
2021-06-17 11:15:05 +0200

    explicitly set baseURI from the daemon into the yarrrml transformer

--- CDE-Box-Daemon/CDE_Transform.rb
+++ CDE-Box-Daemon/CDE_Transform.rb
@@ -7,7 +7,7 @@ class CDE_Transform
   def transform(datatype_tag)
     csv_filename = "/data/#{datatype_tag}.csv"
     return false unless File.exists?("/config/#{datatype_tag}_yarrrml_template.yaml")
-    y = YARRRML_Transform.new(datafile: csv_filename, datatype_tag: datatype_tag)
+    y = YARRRML_Transform.new(datafile: csv_filename, datatype_tag: datatype_tag, baseURI: ENV['baseURI'])
     y.yarrrml_transform
     y.make_fair_data
   end
