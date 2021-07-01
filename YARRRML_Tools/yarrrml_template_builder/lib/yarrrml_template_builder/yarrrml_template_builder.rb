2af77d8e96333cb30cfcf2f9066f9e93911a5e88
Mark Wilkinson <markw@illuminae.com>
2021-07-01 15:25:59 +0200

    added the conforms-to property toattach protocols

--- YARRRML_Tools/yarrrml_template_builder/lib/yarrrml_template_builder/yarrrml_template_builder.rb
+++ YARRRML_Tools/yarrrml_template_builder/lib/yarrrml_template_builder/yarrrml_template_builder.rb
@@ -46,7 +46,7 @@ class YARRRML_Template_Builder
 "role" => ["http://semanticscience.org/resource/SIO_000016", "http://semanticscience.org/resource/role"],
 "process" => ["http://semanticscience.org/resource/SIO_000006", "http://semanticscience.org/resource/process"],
 "attribute" => ["http://semanticscience.org/resource/SIO_000614", "http://semanticscience.org/resource/attribute"],
-"conforms-to" => ["http://semanticscience.org/resource/conforms-to", "http://semanticscience.org/resource/conforms-to"],
+"conforms-to" => ["https://semanticscience.org/resource/CHEMINF_000047", "http://semanticscience.org/resource/conforms-to"],
              
 }
 
