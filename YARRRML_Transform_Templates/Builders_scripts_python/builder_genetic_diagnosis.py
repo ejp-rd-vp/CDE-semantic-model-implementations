e9ff5b301b4e5738985d434dff128f62482bf0fc
pabloalarconm <pabloalarconmoreno@gmail.com>
2022-04-04 20:16:25 +0200

    Fixing basic uri

--- YARRRML_Transform_Templates/Builders_scripts_python/builder_genetic_diagnosis.py
+++ YARRRML_Transform_Templates/Builders_scripts_python/builder_genetic_diagnosis.py
@@ -13,75 +13,75 @@ prefixes = dict(
 triplets = [
 
 # Nodes
-["this:$(pid)_$(uniqid)#ID","sio:SIO_000020","this:$(pid)_$(uniqid)#Genetic_Role","iri"],
-["this:$(pid)_$(uniqid)#Entity","sio:SIO_000228","this:$(pid)_$(uniqid)#Genetic_Role","iri"],
-["this:$(pid)_$(uniqid)#Entity","sio:SIO_000008","$(clinvar_uri)","iri"],
-["this:$(pid)_$(uniqid)#Entity","sio:SIO_000008","$(omim_uri)","iri"],
-["this:$(pid)_$(uniqid)#Entity","sio:SIO_000008","$(hgnc_uri)","iri"],
+["this:$(pid)_$(uniqid)_ID","sio:SIO_000020","this:$(pid)_$(uniqid)_Genetic_Role","iri"],
+["this:$(pid)_$(uniqid)_Entity","sio:SIO_000228","this:$(pid)_$(uniqid)_Genetic_Role","iri"],
+["this:$(pid)_$(uniqid)_Entity","sio:SIO_000008","$(clinvar_uri)","iri"],
+["this:$(pid)_$(uniqid)_Entity","sio:SIO_000008","$(omim_uri)","iri"],
+["this:$(pid)_$(uniqid)_Entity","sio:SIO_000008","$(hgnc_uri)","iri"],
 
-["this:$(pid)_$(uniqid)#Genetic_Role","sio:SIO_000356","this:$(pid)_$(uniqid)#HGVS_Process","iri"],
-["this:$(pid)_$(uniqid)#Genetic_Role","sio:SIO_000356","this:$(pid)_$(uniqid)#OMIM_Process","iri"],
-["this:$(pid)_$(uniqid)#Genetic_Role","sio:SIO_000356","this:$(pid)_$(uniqid)#HGNC_Process","iri"],
+["this:$(pid)_$(uniqid)_Genetic_Role","sio:SIO_000356","this:$(pid)_$(uniqid)_HGVS_Process","iri"],
+["this:$(pid)_$(uniqid)_Genetic_Role","sio:SIO_000356","this:$(pid)_$(uniqid)_OMIM_Process","iri"],
+["this:$(pid)_$(uniqid)_Genetic_Role","sio:SIO_000356","this:$(pid)_$(uniqid)_HGNC_Process","iri"],
 
-["this:$(pid)_$(uniqid)#HGVS_Process","sio:SIO_000229","this:$(pid)_$(uniqid)#HGVS_Output","iri"],
-["this:$(pid)_$(uniqid)#HGVS_Output","sio:SIO_000628","$(clinvar_uri)","iri"],
+["this:$(pid)_$(uniqid)_HGVS_Process","sio:SIO_000229","this:$(pid)_$(uniqid)_HGVS_Output","iri"],
+["this:$(pid)_$(uniqid)_HGVS_Output","sio:SIO_000628","$(clinvar_uri)","iri"],
 
-["this:$(pid)_$(uniqid)#OMIM_Process","sio:SIO_000229","this:$(pid)_$(uniqid)#OMIM_Output","iri"],
-["this:$(pid)_$(uniqid)#OMIM_Output","sio:SIO_000628","$(omim_uri)","iri"],
+["this:$(pid)_$(uniqid)_OMIM_Process","sio:SIO_000229","this:$(pid)_$(uniqid)_OMIM_Output","iri"],
+["this:$(pid)_$(uniqid)_OMIM_Output","sio:SIO_000628","$(omim_uri)","iri"],
 
-["this:$(pid)_$(uniqid)#HGNC_Process","sio:SIO_000229","this:$(pid)_$(uniqid)#HGNC_Output","iri"],
-["this:$(pid)_$(uniqid)#HGNC_Output","sio:SIO_000628","$(hgnc_uri)","iri"],
+["this:$(pid)_$(uniqid)_HGNC_Process","sio:SIO_000229","this:$(pid)_$(uniqid)_HGNC_Output","iri"],
+["this:$(pid)_$(uniqid)_HGNC_Output","sio:SIO_000628","$(hgnc_uri)","iri"],
 
 # Types
-["this:$(pid)_$(uniqid)#ID","rdf:type","sio:SIO_000115","iri"],
-["this:$(pid)_$(uniqid)#Entity","rdf:type","sio:SIO_000498","iri"],
-["this:$(pid)_$(uniqid)#Genetic_Role","rdf:type","sio:SIO_000016","iri"],
-["this:$(pid)_$(uniqid)#Genetic_Role","rdf:type","obo:OBI_0000093","iri"],
+["this:$(pid)_$(uniqid)_ID","rdf:type","sio:SIO_000115","iri"],
+["this:$(pid)_$(uniqid)_Entity","rdf:type","sio:SIO_000498","iri"],
+["this:$(pid)_$(uniqid)_Genetic_Role","rdf:type","sio:SIO_000016","iri"],
+["this:$(pid)_$(uniqid)_Genetic_Role","rdf:type","obo:OBI_0000093","iri"],
 
-["this:$(pid)_$(uniqid)#HGVS_Process","rdf:type","obo:NCIT_C15709","iri"],
-["this:$(pid)_$(uniqid)#HGVS_Process","rdf:type","sio:SIO_000006","iri"],
-["this:$(pid)_$(uniqid)#HGVS_Output","rdf:type","sio:SIO_000015","iri"],
-["this:$(pid)_$(uniqid)#HGVS_Output","rdf:type","sio:SIO_001388","iri"],
+["this:$(pid)_$(uniqid)_HGVS_Process","rdf:type","obo:NCIT_C15709","iri"],
+["this:$(pid)_$(uniqid)_HGVS_Process","rdf:type","sio:SIO_000006","iri"],
+["this:$(pid)_$(uniqid)_HGVS_Output","rdf:type","sio:SIO_000015","iri"],
+["this:$(pid)_$(uniqid)_HGVS_Output","rdf:type","sio:SIO_001388","iri"],
 ["$(clinvar_uri)","rdf:type","sio:SIO_000614","iri"],
 ["$(clinvar_uri)","rdf:type","sio:SIO_000015","iri"],
 ["$(clinvar_uri)","rdf:type","obo:NCIT_C171178","iri"],
 
-["this:$(pid)_$(uniqid)#OMIM_Process","rdf:type","obo:NCIT_C15709","iri"],
-["this:$(pid)_$(uniqid)#OMIM_Process","rdf:type","sio:SIO_000006","iri"],
-["this:$(pid)_$(uniqid)#OMIM_Output","rdf:type","sio:SIO_000015","iri"],
-["this:$(pid)_$(uniqid)#OMIM_Output","rdf:type","sio:SIO_001381","iri"],
+["this:$(pid)_$(uniqid)_OMIM_Process","rdf:type","obo:NCIT_C15709","iri"],
+["this:$(pid)_$(uniqid)_OMIM_Process","rdf:type","sio:SIO_000006","iri"],
+["this:$(pid)_$(uniqid)_OMIM_Output","rdf:type","sio:SIO_000015","iri"],
+["this:$(pid)_$(uniqid)_OMIM_Output","rdf:type","sio:SIO_001381","iri"],
 ["$(omim_uri)","rdf:type","sio:SIO_000614","iri"],
 ["$(omim_uri)","rdf:type","sio:SIO_000015","iri"],
 ["$(omim_uri)","rdf:type","edam:data_1153","iri"],
 
-["this:$(pid)_$(uniqid)#HGNC_Process","rdf:type","obo:NCIT_C15709","iri"],
-["this:$(pid)_$(uniqid)#HGNC_Process","rdf:type","sio:SIO_000006","iri"],
-["this:$(pid)_$(uniqid)#HGNC_Output","rdf:type","sio:SIO_000015","iri"],
-["this:$(pid)_$(uniqid)#HGNC_Output","rdf:type","sio:SIO_001381","iri"],
+["this:$(pid)_$(uniqid)_HGNC_Process","rdf:type","obo:NCIT_C15709","iri"],
+["this:$(pid)_$(uniqid)_HGNC_Process","rdf:type","sio:SIO_000006","iri"],
+["this:$(pid)_$(uniqid)_HGNC_Output","rdf:type","sio:SIO_000015","iri"],
+["this:$(pid)_$(uniqid)_HGNC_Output","rdf:type","sio:SIO_001381","iri"],
 ["$(hgnc_uri)","rdf:type","sio:SIO_000614","iri"],
 ["$(hgnc_uri)","rdf:type","sio:SIO_000015","iri"],
 ["$(hgnc_uri)","rdf:type","edam:data_2298","iri"],
 
 
 # Labels
-["this:$(pid)_$(uniqid)#Genetic_Role","rdfs:label","Role: Genetic diagnosis patient","xsd:string"],
-["this:$(pid)_$(uniqid)#HGVS_Process","rdfs:label","Process: HGVS genetic testing","xsd:string"],
-["this:$(pid)_$(uniqid)#HGVS_Output","rdfs:label","Output type: HGVS measurement value","xsd:string"],
+["this:$(pid)_$(uniqid)_Genetic_Role","rdfs:label","Role: Genetic diagnosis patient","xsd:string"],
+["this:$(pid)_$(uniqid)_HGVS_Process","rdfs:label","Process: HGVS genetic testing","xsd:string"],
+["this:$(pid)_$(uniqid)_HGVS_Output","rdfs:label","Output type: HGVS measurement value","xsd:string"],
 ["$(clinvar_uri)","rdfs:label","Attribute type: $(hgvs_string)","xsd:string"],
 
-["this:$(pid)_$(uniqid)#OMIM_Process","rdfs:label","Process: OMIM genetic testing","xsd:string"],
-["this:$(pid)_$(uniqid)#OMIM_Output","rdfs:label","Output type: OMIM genome sequence variant","xsd:string"],
+["this:$(pid)_$(uniqid)_OMIM_Process","rdfs:label","Process: OMIM genetic testing","xsd:string"],
+["this:$(pid)_$(uniqid)_OMIM_Output","rdfs:label","Output type: OMIM genome sequence variant","xsd:string"],
 ["$(omim_uri)","rdfs:label","Attribute type: $(omim_uri)","xsd:string"],
 
-["this:$(pid)_$(uniqid)#HGNC_Process","rdfs:label","Process: HGNC genetic testing","xsd:string"],
-["this:$(pid)_$(uniqid)#HGNC_Output","rdfs:label","Output type: HGNC genome sequence variant","xsd:string"],
+["this:$(pid)_$(uniqid)_HGNC_Process","rdfs:label","Process: HGNC genetic testing","xsd:string"],
+["this:$(pid)_$(uniqid)_HGNC_Output","rdfs:label","Output type: HGNC genome sequence variant","xsd:string"],
 ["$(hgnc_uri)","rdfs:label","Attribute type: $(hgnc_uri)","xsd:string"],
 
 # Values
-["this:$(pid)_$(uniqid)#ID","sio:SIO_000300","$(pid)","xsd:string"],
-["this:$(pid)_$(uniqid)#HGVS_Output","sio:SIO_000300","$(hgvs_string)","xsd:string"],
-["this:$(pid)_$(uniqid)#OMIM_Output","sio:SIO_000300","$(omim_uri)","xsd:string"],
-["this:$(pid)_$(uniqid)#HGNC_Output","sio:SIO_000300","$(hgnc_uri)","xsd:string"]]
+["this:$(pid)_$(uniqid)_ID","sio:SIO_000300","$(pid)","xsd:string"],
+["this:$(pid)_$(uniqid)_HGVS_Output","sio:SIO_000300","$(hgvs_string)","xsd:string"],
+["this:$(pid)_$(uniqid)_OMIM_Output","sio:SIO_000300","$(omim_uri)","xsd:string"],
+["this:$(pid)_$(uniqid)_HGNC_Output","sio:SIO_000300","$(hgnc_uri)","xsd:string"]]
 
 
 config = dict(
--- YARRRML_Transform_Templates/Builders_scripts_python/builder_patient_status.py
+++ YARRRML_Transform_Templates/Builders_scripts_python/builder_patient_status.py
@@ -12,47 +12,47 @@ prefixes = dict(
 triplets = [
 
 # Nodes
-["this:$(pid)_$(uniqid)#ID","sio:SIO_000020","this:$(pid)_$(uniqid)#Status_Role","iri"],
-["this:$(pid)_$(uniqid)#Entity","sio:SIO_000228","this:$(pid)_$(uniqid)#Status_Role","iri"],
-["this:$(pid)_$(uniqid)#Entity","sio:SIO_000008","this:$(pid)_$(uniqid)#Status_Attribute","iri"],
-["this:$(pid)_$(uniqid)#Status_Role","sio:SIO_000356","this:$(pid)_$(uniqid)#Status_Process","iri"],
-["this:$(pid)_$(uniqid)#Status_Process","sio:SIO_000229","this:$(pid)_$(uniqid)#Status_Output","iri"],
-["this:$(pid)_$(uniqid)#Status_Output","sio:SIO_000628","this:$(pid)_$(uniqid)#Status_Attribute","iri"],
+["this:$(pid)_$(uniqid)_ID","sio:SIO_000020","this:$(pid)_$(uniqid)_Status_Role","iri"],
+["this:$(pid)_$(uniqid)_Entity","sio:SIO_000228","this:$(pid)_$(uniqid)_Status_Role","iri"],
+["this:$(pid)_$(uniqid)_Entity","sio:SIO_000008","this:$(pid)_$(uniqid)_Status_Attribute","iri"],
+["this:$(pid)_$(uniqid)_Status_Role","sio:SIO_000356","this:$(pid)_$(uniqid)_Status_Process","iri"],
+["this:$(pid)_$(uniqid)_Status_Process","sio:SIO_000229","this:$(pid)_$(uniqid)_Status_Output","iri"],
+["this:$(pid)_$(uniqid)_Status_Output","sio:SIO_000628","this:$(pid)_$(uniqid)_Status_Attribute","iri"],
 
-["this:$(pid)_$(uniqid)#Entity","sio:SIO_000008","this:$(pid)_$(uniqid)#Death_information_Attribute","iri"],
-["this:$(pid)_$(uniqid)#Status_Role","sio:SIO_000356","this:$(pid)_$(uniqid)#Death_information_Process","iri"],
-["this:$(pid)_$(uniqid)#Death_information_Process","sio:SIO_000229","this:$(pid)_$(uniqid)#Death_information_Output","iri"],
-["this:$(pid)_$(uniqid)#Death_information_Output","sio:SIO_000628","this:$(pid)_$(uniqid)#Death_information_Attribute","iri"],
+["this:$(pid)_$(uniqid)_Entity","sio:SIO_000008","this:$(pid)_$(uniqid)_Death_information_Attribute","iri"],
+["this:$(pid)_$(uniqid)_Status_Role","sio:SIO_000356","this:$(pid)_$(uniqid)_Death_information_Process","iri"],
+["this:$(pid)_$(uniqid)_Death_information_Process","sio:SIO_000229","this:$(pid)_$(uniqid)_Death_information_Output","iri"],
+["this:$(pid)_$(uniqid)_Death_information_Output","sio:SIO_000628","this:$(pid)_$(uniqid)_Death_information_Attribute","iri"],
 
 # Types
-["this:$(pid)_$(uniqid)#ID","rdf:type","sio:SIO_000115","iri"],
-["this:$(pid)_$(uniqid)#Entity","rdf:type","sio:SIO_000498","iri"],
-["this:$(pid)_$(uniqid)#Status_Role","rdf:type","sio:SIO_000016","iri"],
-["this:$(pid)_$(uniqid)#Status_Role","rdf:type","obo:OBI_0000093","iri"],
-["this:$(pid)_$(uniqid)#Status_Process","rdf:type","sio:SIO_001052","iri"],
-["this:$(pid)_$(uniqid)#Status_Process","rdf:type","sio:SIO_000006","iri"],
-["this:$(pid)_$(uniqid)#Status_Output","rdf:type","sio:SIO_000015","iri"],
-["this:$(pid)_$(uniqid)#Status_Attribute","rdf:type","sio:SIO_000614","iri"],
-["this:$(pid)_$(uniqid)#Status_Attribute","rdf:type","$(status_uri)","iri"],
+["this:$(pid)_$(uniqid)_ID","rdf:type","sio:SIO_000115","iri"],
+["this:$(pid)_$(uniqid)_Entity","rdf:type","sio:SIO_000498","iri"],
+["this:$(pid)_$(uniqid)_Status_Role","rdf:type","sio:SIO_000016","iri"],
+["this:$(pid)_$(uniqid)_Status_Role","rdf:type","obo:OBI_0000093","iri"],
+["this:$(pid)_$(uniqid)_Status_Process","rdf:type","sio:SIO_001052","iri"],
+["this:$(pid)_$(uniqid)_Status_Process","rdf:type","sio:SIO_000006","iri"],
+["this:$(pid)_$(uniqid)_Status_Output","rdf:type","sio:SIO_000015","iri"],
+["this:$(pid)_$(uniqid)_Status_Attribute","rdf:type","sio:SIO_000614","iri"],
+["this:$(pid)_$(uniqid)_Status_Attribute","rdf:type","$(status_uri)","iri"],
 
-["this:$(pid)_$(uniqid)#Death_information_Process","rdf:type","sio:SIO_000006","iri"],
-["this:$(pid)_$(uniqid)#Death_information_Output","rdf:type","sio:SIO_000015","iri"],
-["this:$(pid)_$(uniqid)#Death_information_Attribute","rdf:type","sio:SIO_000614","iri"],
-["this:$(pid)_$(uniqid)#Death_information_Attribute","rdf:type","obo:NCIT_C70810","iri"],
+["this:$(pid)_$(uniqid)_Death_information_Process","rdf:type","sio:SIO_000006","iri"],
+["this:$(pid)_$(uniqid)_Death_information_Output","rdf:type","sio:SIO_000015","iri"],
+["this:$(pid)_$(uniqid)_Death_information_Attribute","rdf:type","sio:SIO_000614","iri"],
+["this:$(pid)_$(uniqid)_Death_information_Attribute","rdf:type","obo:NCIT_C70810","iri"],
 
 # Labels
-["this:$(pid)_$(uniqid)#Status_Role","rdfs:label","Role: Patient for status recording","xsd:string"],
-["this:$(pid)_$(uniqid)#Status_Process","rdfs:label","Process: Status recording process","xsd:string"],
-["this:$(pid)_$(uniqid)#Status_Output","rdfs:label","Output type: $(status_label)","xsd:string"],
-["this:$(pid)_$(uniqid)#Status_Attribute","rdfs:label","Attribute type: $(status_label)","xsd:string"],
-["this:$(pid)_$(uniqid)#Death_information_Process","rdfs:label","Process: Death information recording process","xsd:string"],
-["this:$(pid)_$(uniqid)#Death_information_Output","rdfs:label","Output type: Patient death information)","xsd:string"],
-["this:$(pid)_$(uniqid)#Death_information_Attribute","rdfs:label","Attribute type: Date of death","xsd:string"],
+["this:$(pid)_$(uniqid)_Status_Role","rdfs:label","Role: Patient for status recording","xsd:string"],
+["this:$(pid)_$(uniqid)_Status_Process","rdfs:label","Process: Status recording process","xsd:string"],
+["this:$(pid)_$(uniqid)_Status_Output","rdfs:label","Output type: $(status_label)","xsd:string"],
+["this:$(pid)_$(uniqid)_Status_Attribute","rdfs:label","Attribute type: $(status_label)","xsd:string"],
+["this:$(pid)_$(uniqid)_Death_information_Process","rdfs:label","Process: Death information recording process","xsd:string"],
+["this:$(pid)_$(uniqid)_Death_information_Output","rdfs:label","Output type: Patient death information)","xsd:string"],
+["this:$(pid)_$(uniqid)_Death_information_Attribute","rdfs:label","Attribute type: Date of death","xsd:string"],
 
 # Values
-["this:$(pid)_$(uniqid)#ID","sio:SIO_000300","$(pid)","xsd:string"],
-["this:$(pid)_$(uniqid)#Status_Output","sio:SIO_000300","$(status_label)","xsd:string"],
-["this:$(pid)_$(uniqid)#Death_information_Output","sio:SIO_000300","$(death_date)","xsd:date"]]
+["this:$(pid)_$(uniqid)_ID","sio:SIO_000300","$(pid)","xsd:string"],
+["this:$(pid)_$(uniqid)_Status_Output","sio:SIO_000300","$(status_label)","xsd:string"],
+["this:$(pid)_$(uniqid)_Death_information_Output","sio:SIO_000300","$(death_date)","xsd:date"]]
 
 
 config = dict(
