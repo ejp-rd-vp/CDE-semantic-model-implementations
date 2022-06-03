99371151a4262d07b47ab6c865178cc2b686869d
Mark Wilkinson <markw@illuminae.com>
2022-06-03 10:32:18 +0200

    code cleanup

--- CDE-Box-Daemon/CDE_Transform.rb
+++ CDE-Box-Daemon/CDE_Transform.rb
@@ -1,12 +1,12 @@
 require 'yarrrml-template-builder'
-require 'ffi'  #ffi-1.14.2
+require 'ffi'  # ffi-1.14.2
 require 'json'
 
-class CDE_Transform  
-
+class CDE_Transform
   def transform(datatype_tag)
     csv_filename = "/data/#{datatype_tag}.csv"
-    return false unless File.exists?("/config/#{datatype_tag}_yarrrml_template.yaml")
+    return false unless File.exist?("/config/#{datatype_tag}_yarrrml_template.yaml")
+
     y = YARRRML_Transform.new(datafile: csv_filename, datatype_tag: datatype_tag, baseURI: ENV['baseURI'])
     y.yarrrml_transform
     y.make_fair_data
--- CDE-Box-Daemon/transform.rb
+++ CDE-Box-Daemon/transform.rb
@@ -1,4 +1,3 @@
-
 require './CDE_Transform'
 require 'sinatra'
 require 'rest-client'
@@ -7,78 +6,70 @@ require 'open3'
 
 include HTTPUtils
 
-
 get '/' do
-  update()
-  execute()
-  cleanup()
-  metadata_update()
+  update
+  execute
+  cleanup
+  metadata_update
   "Execution complete.  See docker log for errors (if any)\n\n"
-
 end
 
 def update
-  $stderr.puts "first open3 git pull"
-  o, e, s = Open3.capture3("cd CDE-semantic-model-implementations && git pull")
-  $stderr.puts "second open3 copy yarrrml #{o}  #{e}"
-  s = s
-  o, e, s = Open3.capture3("cp -rf ./CDE-semantic-model-implementations/YARRRML_Transform_Templates/templates/*.yaml  /config")
-  $stderr.puts "second open3 complete #{o} #{e}"
-    
+  warn 'first open3 git pull'
+  o, e, _s = Open3.capture3('cd CDE-semantic-model-implementations && git pull')
+  warn "second open3 copy yarrrml #{o}  #{e}"
+  o, e, _s = Open3.capture3('cp -rf ./CDE-semantic-model-implementations/YARRRML_Transform_Templates/templates/*.yaml  /config')
+  warn "second open3 complete #{o} #{e}"
 end
 
 def metadata_update
-  $stderr.puts "calling metadata updater image"
-  resp = RestClient.get("http://updater:4567/update")
-  $stderr.puts resp
-  $stderr.puts "\n\nMetadata Update complete\n\n"    
+  warn 'calling metadata updater image'
+  resp = RestClient.get('http://updater:4567/update')
+  warn resp
+  warn "\n\nMetadata Update complete\n\n"
 end
 
-
 def cleanup
-  $stderr.puts "closing cleanup open3"
-      o, s = Open3.capture2("rm -rf /data/triplesstats.csv")
-      o = o; s = s
+  warn 'closing cleanup open3'
+  _o, _s = Open3.capture2('rm -rf /data/triplesstats.csv')
 end
 
 def execute
-  
-  datatype_list = Dir["/data/*.csv"]
-  
-  #datatype_list = %w{personal}
-  
-  cde = CDE_Transform.new()
-    
+  purge_nt
+  datatype_list = Dir['/data/*.csv']
+  # datatype_list = %w{personal}
+  cde = CDE_Transform.new
   datatype_list.each do |d|
-    d =~ /.+\/([^\.]+)\.csv/
-    datatype = $1
+    d =~ %r{.+/([^.]+)\.csv}
+    datatype = Regexp.last_match(1)
     next unless datatype
+
     cde.transform(datatype)
   end
 
-
-  begin
-    File.delete("/data/triples/*.nt")
-  rescue
-    $stderr.puts ""
-  ensure
-    $stderr.puts "looks like it is already clean in here!"
+  files = Dir['/data/triples/*.nt']
+  concatenated = ''
+  files.each do |f|
+    content = File.open(f, 'r').read
+    concatenated += content
   end
-  
-  files = Dir["/data/triples/*.nt"]
-  concatenated = ""
-  files.each {|f| content = File.open(f, "r").read; concatenated += content}
-  #f =File.open("/data/triples/concatenated.nt", "w") 
-  #f.write concatenated
-  #f.close
-  #
+  write_to_graphdb(concatenated)
+end
+
+def write_to_graphdb(concatenated)
   user = ENV['GraphDB_User']
   pass = ENV['GraphDB_Pass']
-  network = "graphdb"
-  network = ENV['networkname'] if ENV['networkname']
+  network = ENV['networkname'] || 'graphdb'
   url = "http://#{network}:7200/repositories/cde/statements"
-  headers = { :content_type => "application/n-triples" }
+  headers = { content_type: 'application/n-triples' }
 
   HTTPUtils.put(url, headers, concatenated, user, pass)
+end
 
+def purge_nt
+  File.delete('/data/triples/*.nt')
+rescue StandardError
+  warn ''
+ensure
+  warn 'looks like it is already clean in here!'
 end
